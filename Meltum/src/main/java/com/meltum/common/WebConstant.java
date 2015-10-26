fd08f96b5057d14c6e972b3d5e9c462f8307ed59
Jérome <Jrome@Djey>
2015-10-26 19:20:10 +0200

    initial commit

---
+++ Meltum/src/main/java/com/meltum/common/WebConstant.java
@@ -0,0 +1,7 @@
+package com.meltum.common;
+
+public interface WebConstant {
+
+	public static final String HOME_VIEW = "home";
+	public static final String ADMIN_VIEW = "admin";
+}
---
+++ Meltum/src/main/java/com/meltum/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.meltum.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.meltum.common.WebConstant;
+ 
+@Controller
+@RequestMapping("/home")
+public class HomeController { 
+	
+	@RequestMapping(value = { "", "/toto" }, method = RequestMethod.GET)
+	public String display() {
+		return WebConstant.HOME_VIEW;
+	}
+	
+	@RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
+	public String protec() {
+		return WebConstant.ADMIN_VIEW;
+	}
+	
+}
